dbea77d559634f6d5412d2893f84f4dc266e231f
Makkah Beasley <makkahbeasley@gmail.com>
2021-08-10 17:02:26 -0400

    Beer List edits 2

--- vue/src/views/Beers.vue
+++ vue/src/views/Beers.vue
@@ -6,7 +6,11 @@
 
       <h1>Urban Artifact</h1>
       <h3>THE GADGET</h3>
-      <h3><img src="~@/assets/images/beer-1692444_2ddfc_sm.jpeg" /></h3>
+      <h3>
+        <img
+          src="~@/assets/images/ddb54c4e150f31442d898ad207058094_640x640.jpg"
+        />
+      </h3>
       <h3>
         A Midwest Fruit Tart Ale with 1,280 lbs of blackberries, 1,280 lbs of
         raspberries and 30 g of vanilla beans per 30 BBL batch Show Less
@@ -23,7 +27,11 @@
 
       <h1>Fibonacci Brewing Company</h1>
       <h3>The Tollhouse</h3>
-      <h3><img src="~@/assets/images/Tollhouse@2x.png" /></h3>
+      <h3>
+        <img
+          src="~@/assets/images/21e8a8fb14729c3793491671ce3c6dc7_640x640.jpg"
+        />
+      </h3>
       <h3>
         Jet black color with moderate bitterness. Strong roasted coffee flavor
         and aroma with hints of chocolate.
@@ -40,7 +48,9 @@
 
       <h1>Rhinegeist Brewery</h1>
       <h3>ANDROMEDA</h3>
-      <h3><img src="~@/assets/images/Pale-Ale_-2-117x200.png" /></h3>
+      <h3>
+        <img src="~@/assets/images/ca27e079a7a4ecd26389ca2fb8ec5190.jpg" />
+      </h3>
       <h3>
         Brewed once upon a time, in a galaxy not so far away. Andromeda Pale Ale
         casts a constellation of interstellar hop flavor, illuminating the
@@ -59,9 +69,14 @@
 
       <h1>Darkness Brewing</h1>
       <h3>Tugg Speedman</h3>
-      <h3><img src="~@/assets/images/GMwKIalm_400x400.jpg" /></h3>
       <h3>
-        Jucy and easy to drink session (lower alcohol) brewed with Sabro, El Dorado, and Mosaic hops for a wonderfully refreshing summer IPA
+        <img
+          src="~@/assets/images/201dfd885220e067b360a6bfe569cac1_640x640.jpg"
+        />
+      </h3>
+      <h3>
+        Jucy and easy to drink session (lower alcohol) brewed with Sabro, El
+        Dorado, and Mosaic hops for a wonderfully refreshing summer IPA
       </h3>
       <h3>4.8% ABV</h3>
       <h3>Session IPA</h3>
@@ -75,9 +90,12 @@
 
       <h1>Bircus Brewing Company</h1>
       <h3>Juggleoats</h3>
-      <h3><img src="~@/assets/images\upload_vwaPtL-squareMedium.png" /></h3>
+      <h3><img src="~@/assets/images/Juggleoats-01.jpg" /></h3>
       <h3>
-        For our newest IPA launch we packed Juggleoats with over 30% flaked oats for a creamy, smooth drinking Oatmeal IPA. A blend of 4 hop varieties bring a pleasant and well balanced bitterness to this unique, milky beer. We serve on Nitro to further enhance the mouthfeel of this beer.
+        For our newest IPA launch we packed Juggleoats with over 30% flaked oats
+        for a creamy, smooth drinking Oatmeal IPA. A blend of 4 hop varieties
+        bring a pleasant and well balanced bitterness to this unique, milky
+        beer. We serve on Nitro to further enhance the mouthfeel of this beer.
         NO LACTOSE!
       </h3>
       <h3>6.2% ABV 60 IBU</h3>
@@ -91,141 +109,189 @@
       </h3>
 
       <h1>Fifty West Brewing Company</h1>
-      <h3>Name</h3>
-      <h3><img src="" /></h3>
-      <h3>Description</h3>
-      <h3>ABV</h3>
-      <h3>Beer Type</h3>
+      <h3>Doom Pedal</h3>
+      <h3>
+        <img
+          src="~@/assets/images/50-west-brewing-co-doom-pedal-white-ale.jpg"
+        />
+      </h3>
+      <h3>
+        Brewed in memory of the late Allen White, a close friend and family
+        member of Fifty West Brewery. This Belgian style beer pours a hazy
+        blonde and is made with coriander and orange peel. A crisp wheat with
+        sweet citrus notes.
+      </h3>
+      <h3>5.5% ABV</h3>
+      <h3>Belgian</h3>
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://fiftywestbrew.com/beers/'"
           value="Click Here For Full Beer List"
         />
       </h3>
 
       <h1>Brink Brewing Co</h1>
-      <h3>Name</h3>
-      <h3><img src="" /></h3>
-      <h3>Description</h3>
-      <h3>ABV</h3>
-      <h3>Beer Type</h3>
+      <h3>No Way In Helles</h3>
+      <h3>
+        <img
+          src="~@/assets/images/8073891e03dab7559da6035f7f625edb_640x640.jpg"
+        />
+      </h3>
+      <h3>
+        Traditional German style Munich Helles Lager or Pale Malty European
+        Lager. Very similar to a pilsner with slightly more body and grainy
+        malty sweetness that finishes softer. - GLUTEN REDUCED
+      </h3>
+      <h3>5.5% ABV</h3>
+      <h3>Helles Lager</h3>
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://www.brinkbrewing.com/beers'"
           value="Click Here For Full Beer List"
         />
       </h3>
 
       <h1>Fretboard Brewing Company</h1>
-      <h3>Name</h3>
-      <h3><img src="" /></h3>
-      <h3>Description</h3>
-      <h3>ABV</h3>
-      <h3>Beer Type</h3>
+      <h3>Rockin The Rhein</h3>
+      <h3><img src="~@/assets/images/442388.jpg" /></h3>
+      <h3>Toasted biscuit and balanced sweet malt</h3>
+      <h3>5.5% ABV</h3>
+      <h3>Octoberfest Lager</h3>
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://fretboardbrewing.com/on-tap'"
           value="Click Here For Full Beer List"
         />
       </h3>
 
       <h1>HighGrain Brewing Co</h1>
-      <h3>Name</h3>
-      <h3><img src="" /></h3>
-      <h3>Description</h3>
-      <h3>ABV</h3>
-      <h3>Beer Type</h3>
+      <h3>Titan</h3>
+      <h3>
+        <img
+          src="~@/assets/images/f86f8e69-6729-4091-93b3-6a977a7e7635-aBrew13.jpg"
+        />
+      </h3>
+      <h3>
+        Piney.Dank.Citrus. An aggressively classic American double dry hopped
+        West Coast IPA, with flavors as big and bold as the titans of the West.
+      </h3>
+      <h3>7.3% ABV</h3>
+      <h3>West Coast IPA</h3>
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://highgrainbrewing.com/drink-menu'"
           value="Click Here For Full Beer List"
         />
       </h3>
 
       <h1>March First Brewing and Distilling</h1>
-      <h3>Name</h3>
-      <h3><img src="" /></h3>
-      <h3>Description</h3>
-      <h3>ABV</h3>
-      <h3>Beer Type</h3>
+      <h3>CRAFT LAGER</h3>
+      <h3><img src="~@/assets/images/Craft-Lager.jpeg" /></h3>
+      <h3>
+        This easy drinking lager pours a bright and lively pale golden color. A
+        smooth, malty aroma gives way to light bodied and bubbly session beer,
+        great for any occasion
+      </h3>
+      <h3>4.7% ABV</h3>
+      <h3>Lager</h3>
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://www.marchfirstbrewing.com/beer'"
           value="Click Here For Full Beer List"
         />
       </h3>
 
       <h1>West Side Brewing</h1>
-      <h3>Name</h3>
-      <h3><img src="" /></h3>
-      <h3>Description</h3>
-      <h3>ABV</h3>
-      <h3>Beer Type</h3>
+      <h3>Barleywine</h3>
+      <h3><img src="~@/assets/images/westside-image-beer-barleywine.jpg" /></h3>
+      <h3>
+        Brewed once a year in December for a release the following January 1st,
+        our Barleywine-style ale is a beer to drink fresh or age. Intense with a
+        subtle date-like fruitiness that goes perfectly with a big, layered
+        malty body. Classic American hops add firm bitterness and a bit of
+        dryness to the finish to help balance the malt.
+      </h3>
+      <h3>11% ABV</h3>
+      <h3>Ale</h3>
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://www.westsidebrewing.com/beers/'"
           value="Click Here For Full Beer List"
         />
       </h3>
 
       <h1>MadTree Brewing</h1>
-      <h3>Name</h3>
-      <h3><img src="" /></h3>
-      <h3>Description</h3>
-      <h3>ABV</h3>
-      <h3>Beer Type</h3>
+      <h3>Katlyn's Lolo Tango</h3>
+      <h3>
+        <img
+          src="~@/assets/images/e01b6e0a2e71f6e453838bac6da3916f_640x640.jpg"
+        />
+      </h3>
+      <h3>
+        Citrus. Tropical. Smooth. Inspired by a pale ale from her travels,
+        Katlyn’s beer is perfect for her favorite summertime activity; taking it
+        easy while hanging by the pool. If easy-drinking and chilling is your
+        jam, then this beer is for you.
+      </h3>
+      <h3>5% ABV</h3>
+      <h3>Pale Ale</h3>
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://www.madtreebrewing.com/beer/'"
           value="Click Here For Full Beer List"
         />
       </h3>
 
       <h1>Rolling Mill Brewing Company</h1>
-      <h3>Name</h3>
-      <h3><img src="" /></h3>
-      <h3>Description</h3>
-      <h3>ABV</h3>
-      <h3>Beer Type</h3>
+      <h3>Two Goldens</h3>
+      <h3><img src="~@/assets/images/RollingMill_web_5.jpg" /></h3>
+      <h3>
+        Light, crisp, and easy to drink American Lager Little to no hop aroma or
+        flavor.
+      </h3>
+      <h3>4.2% ABV</h3>
+      <h3>American Lager</h3>
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://www.rollingmillbc.com/our-beer'"
           value="Click Here For Full Beer List"
         />
       </h3>
 
       <h1>Listermann Brewing Company</h1>
-      <h3>Name</h3>
-      <h3><img src="" /></h3>
-      <h3>Description</h3>
-      <h3>ABV</h3>
-      <h3>Beer Type</h3>
+      <h3>
+        I've Had It With These Motherf****** IPAs At This Motherf****** Brewery
+      </h3>
+      <h3><img src="~@/assets/images/062d33358e3e224b58ba5c2302a272eb_640x640.jpg" /></h3>
+      <h3>NE IPA with vic secret, mosaic, and simcoe</h3>
+      <h3>5.5% ABV</h3>
+      <h3>IPA</h3>
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://www.listermannbrewing.com/'"
           value="Click Here For Full Beer List"
         />
       </h3>
 
       <h1>Braxton Brewing Company</h1>
-      <h3>Name</h3>
-      <h3><img src="" /></h3>
-      <h3>Description</h3>
-      <h3>ABV</h3>
-      <h3>Beer Type</h3>
+      <h3>SUMMERTRIP</h3>
+      <h3><img src="~@/assets/images/Braxton-Summertrip-2.jpg" /></h3>
+      <h3>This tart German style ale features 50% wheat malt and is balanced from the acidity of passionfruit. Very little to no hop presence allow the bready/biscuit characteristics to come through, with a nice bright Passion Fruit finish.</h3>
+      <h3>4.2% ABV</h3>
+      <h3>Ale</h3>
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://www.braxtonbrewing.com/our-beers/'"
           value="Click Here For Full Beer List"
         />
       </h3>
@@ -267,7 +333,7 @@
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://taftsbeer.com/beer/'"
           value="Click Here For Full Beer List"
         />
       </h3>
@@ -281,7 +347,7 @@
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href='https://streetsidebrewery.com/'"
           value="Click Here For Full Beer List"
         />
       </h3>
@@ -295,7 +361,7 @@
       <h3>
         <input
           type="button"
-          onClick="location.href='https://www.artifactbeer.com/beer/'"
+          onClick="location.href=''"
           value="Click Here For Full Beer List"
         />
       </h3>
